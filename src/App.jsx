87e6da792b5419ce83f31e1297d8263b4dd35ac7
이은정 <ej2grateful@gmail.com>
2025-08-06 12:53:59 +0900

    re

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 
 const App = () => {
-  return <div className="bg-sky-100">App</div>;
+  return (
+    <div className="bg-sky-100 h-screen flex justify-center items-center">
+      App
+    </div>
+  );
 };
 
 export default App;
